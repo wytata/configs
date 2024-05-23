cdd523226b35f1794c721fc5b779594862176c13
Wyatt Cade <cadewya@tamu.edu>
2024-05-23 16:26:19 -0500

    Needed to update for a while

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -5,7 +5,10 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-
+#counter=0
+#i3-msg -t get_workspaces | tr ',' '\n' | sed -nr 's/"name":"([^"]+)"/\1/p' | while read -r name; do
+#  printf 'ws-icon-%i = "%s;<insert-icon-here>"\n' $((counter++)) $name
+#done
 # Launch polybar
 polybar &
 
